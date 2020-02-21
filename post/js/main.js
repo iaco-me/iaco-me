baec54f4a6b0f0e9ee803338f8bf712b22248143
iaco-me <50894914+iaco-me@users.noreply.github.com>
2020-02-21 15:51:30 +0100

    Update main.js

--- post/js/main.js
+++ post/js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function ($) {
-    $('.set-bg').each(function () {
+    $('.config-bg').each(function () {
         var bg = $(this).data('setbg');
         $(this).css('background-image', 'url(' + bg + ')');
     });
