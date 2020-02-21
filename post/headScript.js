ea42331268c45f660a90fb0461bb0f5e76bbcf52
iaco-me <50894914+iaco-me@users.noreply.github.com>
2020-02-21 14:52:55 +0100

    Update headScript.js

--- post/headScript.js
+++ post/headScript.js
@@ -22,6 +22,17 @@ function setTitle(){
 function configurePage(){
   $('#author').load('/post/author.html');
   $('#footer').load('/post/footer.html');
+  $('head').append('
+                  <link rel="apple-touch-icon" sizes="180x180" href="/icon/apple-touch-icon.png">
+                  <link rel="icon" type="image/png" sizes="32x32" href="/icon/favicon-32x32.png">
+                  <link rel="icon" type="image/png" sizes="16x16" href="/icon/favicon-16x16.png">
+                  <link rel="manifest" href="/icon/site.webmanifest">
+                  <link rel="mask-icon" href="/icon/safari-pinned-tab.svg" color="#141414">
+                  <link rel="shortcut icon" href="/icon/favicon.ico">
+                  <meta name="msapplication-TileColor" content="#141414">
+                  <meta name="msapplication-config" content="/icon/browserconfig.xml">
+                  <meta name="theme-color" content="#141414">
+                  ');
 }
 function loadPosts(){
   $('#list').load('/post/list.html');
