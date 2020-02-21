f2a121d32f684c180abecd76b09e6f481d2d03b0
iaco-me <50894914+iaco-me@users.noreply.github.com>
2020-02-21 14:45:32 +0100

    Create postheadScript.js

---
+++ postheadScript.js
@@ -0,0 +1,31 @@
+function setTitle(){
+  var fullTitle = document.getElementsByTagName('title')[0].innerHTML;
+
+  var regexTags = /\[(.*?)\]/g;
+  var tags = '';
+  for (var match of fullTitle.matchAll(regexTags)) {
+    tags += '<span>' + match[1] + '</span>';
+  }
+  document.getElementById('mainTags').innerHTML = tags;
+
+  var regexTitle = /^(.+?)[\n#\[]/;
+  var title = fullTitle.match(regexTitle)[1];
+  document.getElementById('mainTitle').innerHTML = title;
+
+  var regexBTags = /#(\w+)/g;
+  var bTags = '';
+  for (var match of fullTitle.matchAll(regexBTags)) {
+    bTags += '<a>' + match[1] + '</a>';
+  }
+  document.getElementById('bottomTags').innerHTML = bTags;
+}
+function configurePage(){
+  $('#author').load('/post/author.html');
+  $('#footer').load('/post/footer.html');
+}
+function loadPosts(){
+  $('#list').load('/post/list.html');
+}
+function loadPosts(){
+  $('#headImport').load('/post/head.html')
+}
