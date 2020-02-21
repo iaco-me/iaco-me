349bce432d92d40ffe535f3e283d55356c41e74c
iaco-me <50894914+iaco-me@users.noreply.github.com>
2020-02-21 18:09:00 +0100

    Update headScript.js

--- post/headScript.js
+++ post/headScript.js
@@ -29,24 +29,24 @@ function configurePage(){
 function loadPosts(){
   var MAX_POSTS = 2;
   var set = new Set([1]);
-  
-  $('#list').load('/post/list.html');
 
-  while(set.size < 3 && MAX_POSTS >= 3){
-  	var random = Math.floor(Math.random() * (MAX_POSTS)) + 1;
-  	set.add(random);
-  }
-  
-  var index = 0;
-  set.forEach(function(val){
-  	index += 1;
-  	var post = 'https://iaco.me/post/' + ("00" + val).slice(-3);
-    
-    $.get(post, function(data){
-      var title = $(data).filter('title').text().replace(/[#\[](.*)/g, '');
-      $('#title-' + index).html(title);
-  	});
-    $('#img-' + index).attr('src', post + '/cover.jpg');
-    $('#link-' + index).attr('href', post);
+  $('#list').load('/post/list.html', function(){
+    while(set.size < 3 && MAX_POSTS >= 3){
+      var random = Math.floor(Math.random() * (MAX_POSTS)) + 1;
+      set.add(random);
+    }
+
+    var index = 0;
+    set.forEach(function(val){
+      index += 1;
+      var post = 'https://iaco.me/post/' + ("00" + val).slice(-3);
+
+      $.get(post, function(data){
+        var title = $(data).filter('title').text().replace(/[#\[](.*)/g, '');
+        $('#title-' + index).html(title);
+      });
+      $('#img-' + index).attr('src', post + '/cover.jpg');
+      $('#link-' + index).attr('href', post);
+    });
   });
 }
